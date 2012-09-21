4580c7d1cebc88ff639dbdafc12eeea606b5fb8c
A. Akbar Hidayat <keripix@gmail.com>
2012-09-21 14:26:02 +0700

    gambar dah siap

--- freedraw/freedraw.js
+++ freedraw/freedraw.js
@@ -3,13 +3,10 @@
       kotak = document.getElementById('rect'),
       lingkaran = document.getElementById('circle'),
       garis = document.getElementById('line'),
+      gambar = document.getElementById('image'),
       addListener = fabric.util.addListener,
       drawing,
       instance;
-    
-  addListener(kotak, 'click', create);
-  addListener(lingkaran, 'click', create);
-  addListener(line, 'click', create);
   
   function create(e) {
     var id = e.target.id;
@@ -18,6 +15,11 @@
       type: id
     });
   }
+  
+  addListener(kotak, 'click', create);
+  addListener(lingkaran, 'click', create);
+  addListener(garis, 'click', create);
+  addListener(gambar, 'click', create);
   
   function activateDrawing(params) {
     canvas.defaultCursor = 'crosshair';
@@ -28,6 +30,8 @@
       drawing = new Circle();
     } else if (params.type === 'line') {
       drawing = new Line();
+    } else if (params.type === 'image') {
+      drawing = new FImage();
     }
     
     if (drawing) {
@@ -129,21 +133,66 @@
   /******************************************
    * OBJEK UNTUK GAMBAR
    ******************************************/
-  
-  
   function FImage(params) {
-    this.fill = params.fill || 'blue';
-    this.originalWidth = 5;
-    this.originalHeight = 5;
-    this.instance = undefined;
-  }
-  
-  FImage.prototype.create = function () {
     
   }
   
-  FImage.prototype.update = function () {
+  FImage.prototype.create = function (e) {
+    var point = canvas.getPointer(e.e);
     
+    fabric.Image.fromURL('kucing.jpg', function (img) {
+      instance = img;
+      instance.set({
+        top: point.y,
+        left: point.x,
+        height: 4,
+        width: 4
+      });
+      instance.setCoords();
+      canvas.add(instance);
+      canvas.renderAll();
+    }
+    /*,
+    {
+      top: point.y,
+      left: point.x,
+      height: 4,
+      width: 4
+    }*/);
+  }
+  
+  FImage.prototype.update = function (e) {
+    if (instance instanceof fabric.Image) {
+      var point = canvas.getPointer(e.e),
+          topLeft = instance.get('oCoords').tl,
+          width = Math.abs(point.x - topLeft.x),
+          height = Math.abs(point.y - topLeft.y),
+          top,
+          left;
+          
+      if (point.x < topLeft.x && point.y < topLeft.y) {
+        top = height / 2 + point.y;
+        left = width / 2 + point.x;
+      } else if (point.x < topLeft.x) {
+        top = height / 2 + topLeft.y;
+        left = width / 2 + point.x;
+      } else if (point.y < topLeft.y) {
+        top = height / 2 + point.y;
+        left = width / 2 + topLeft.x;
+      } else {
+        top = height / 2 + topLeft.y;
+        left = width / 2 + topLeft.x;
+      }
+      
+      instance.set({
+        top: top,
+        left: left,
+        width: width,
+        height: height
+      });
+      
+      canvas.renderAll();
+    }
   }
   
   /******************************************
